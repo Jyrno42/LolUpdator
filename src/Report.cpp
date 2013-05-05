56cf9cee0dfa1f28b90563fa9ba5a84557fe0cee
Jyrno Ader <jyrno42@gmail.com>
2013-05-05 21:30:47 +0300

    Add threads and a timers to Report.

--- src/Report.cpp
+++ src/Report.cpp
@@ -8,34 +8,6 @@ void Report::GetReport()
 
 	std::stringstream fileNameStream;
 	fileNameStream << reportDirectory << "report_" << timeStamp << ".json";
-
-	/*
-	rapidjson::Value tObj(rapidjson::kObjectType);
-	for(std::map<std::string, std::string >::iterator i = timers.begin(); i != timers.end(); ++i)
-	{
-		rapidjson::Value key((*i).first.c_str());
-		rapidjson::Value val((*i).second.c_str());
-		tObj.AddMember(key, val, allocator);
-	}
-	doc["timers"] = tObj;
-	
-	rapidjson::Value thObj(rapidjson::kObjectType);
-	for(std::map<std::string, ThreadInfo >::iterator i = threads.begin(); i != threads.end(); ++i)
-	{
-		rapidjson::Value key((*i).first.c_str());
-
-		rapidjson::Value tElement;
-		tElement.SetObject();
-		
-		tElement["started_threads"].SetInt((*i).second.started_threads);
-		tElement["finished_threads"].SetInt((*i).second.finished_threads);
-		tElement["summoners"].SetInt((*i).second.summoners);
-
-		tElement["time"] = (*i).second.time.c_str();
-
-		thObj.AddMember(key, tElement, allocator);
-	}
-	doc["threads"] = thObj;*/
 	
 	FILE * file = fopen(fileNameStream.str().c_str(), "w");
 
@@ -63,6 +35,33 @@ void Report::GetReport()
 			arr.PushBack(val, allocator);
 		}
 		doc.AddMember("errors", arr, allocator);
+
+		rapidjson::Value tObj(rapidjson::kObjectType);
+		for(std::map<std::string, std::string >::iterator i = timers.begin(); i != timers.end(); ++i)
+		{
+			rapidjson::Value key((*i).first.c_str());
+			rapidjson::Value val((*i).second.c_str());
+			tObj.AddMember(key, val, allocator);
+		}
+		doc.AddMember("timers", tObj, allocator);
+
+		rapidjson::Value thObj(rapidjson::kObjectType);
+		for(std::map<std::string, ThreadInfo >::iterator i = threads.begin(); i != threads.end(); ++i)
+		{
+			rapidjson::Value key((*i).first.c_str());
+
+			rapidjson::Value tElement;
+			tElement.SetObject();
+		
+			tElement["started_threads"].SetInt((*i).second.started_threads);
+			tElement["finished_threads"].SetInt((*i).second.finished_threads);
+			tElement["summoners"].SetInt((*i).second.summoners);
+
+			tElement["time"] = (*i).second.time.c_str();
+
+			thObj.AddMember(key, tElement, allocator);
+		}
+		doc.AddMember("threads", thObj, allocator);
 
 		// Convert JSON document to string
 		rapidjson::StringBuffer strBuf;
